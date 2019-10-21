bace6fa7f1151f0da9f3d4bba767368d9040ad98
Simeon <simeon@u.northwestern.edu>
2019-10-21 12:07:30 -0400

    init innit?

---
+++ cron.js
@@ -0,0 +1,56 @@
+const request = require("request")
+const cron = require("node-cron")
+const fs = require("fs")
+require("dotenv").config()
+let activityList = require("./activityList.json")
+
+const requestUrl = "https://habitica.com/api/v3/groups/" + process.env.GROUPID + "/chat"
+
+const chatRequest = {
+    url: requestUrl,
+    headers: {
+        "x-api-user": process.env.APIUSER,
+        "x-api-key": process.env.APIKEY
+    }
+}
+
+function updateActivity(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        const chat = Object.entries(JSON.parse(body).data)
+        chat.reverse().forEach(message => {
+            console.log(message)
+            const text = message[1].text
+            if (message[1].info.type === "spell_cast_party") {
+                let caster = text.substr(0, message[1].text.indexOf("casts"))
+                const time = new Date(message[1].timestamp)
+                const now =
+                    time.getMonth() +
+                    1 +
+                    "/" +
+                    time.getDate() +
+                    " " +
+                    time.getHours() +
+                    ":" +
+                    time.getMinutes()
+                activityList[caster] = now
+            }
+        })
+    }
+
+    fs.writeFile(
+        "./activityList.json",
+        JSON.stringify(activityList, null, 4),
+        err => {
+            if (err) throw err
+
+            // success case, the file was saved
+            console.log("json saved")
+        }
+    )
+}
+
+request(chatRequest, updateActivity)
+
+cron.schedule("0,30 0-23 * * *", () => {
+    request(chatRequest, updateActivity)
+})
---
+++ readme.txt
@@ -0,0 +1,4 @@
+a webpage bysimeon, for super active daily quest party
+displays habitica party member most recent spell casts and (soon) more
+-
+uses node-cron to pull chat log twice an hour, creates a .json file which is rendered into html onload
